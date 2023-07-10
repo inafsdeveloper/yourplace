ecd37d09dca787bf0477cf78f100c023773dde30
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-09 20:07:51 -0700

    Implemented Place filters by using react hook (useParams) to get current user id from url

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = require('../../shared/data/places.json'); 
 const UserPlaces = props => {
 
-    return <PlaceList items={DUMMY_PLACES} />
+    const USER_ID = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === USER_ID);
+    return <PlaceList items={loadedPlaces} />
 }
 
 export default UserPlaces;
