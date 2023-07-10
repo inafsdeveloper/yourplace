dca457ac45cd77b51335f11a5d315d4094fc3cac
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-09 18:39:11 -0700

    Adding a UserListPage / Component

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import './UsersList.css';
+import UserItem from "./UserItem";
+
+const UsersList = props => {
+    if (props.items.length === 0) {
+        return (
+            <div className="center">
+                <h2> No Users Found.</h2>
+            </div>
+        );
+    }
+
+    return (
+        <u2>
+            {props.items.map(user => {
+                <UserItem
+                    key={user.id}
+                    id={user.id}
+                    image={user.image}
+                    name={user.name}
+                    placeCount={user.places}
+                />
+            })}
+        </u2>
+    );
+};
+
+export default UsersList;
