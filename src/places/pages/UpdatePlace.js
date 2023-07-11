d8676420d07c491a93f0f3bf192f51184a33c574
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-10 20:06:21 -0700

    Createa Custom Form Hook(useForm)

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -4,48 +4,69 @@ import { useParams } from 'react-router-dom';
 import Input from '../../shared/components/FormElements/Input';
 import Button from "../../shared/components/FormElements/Button";
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators'
-
-
+import {useForm} from '../../shared/hooks/form-hook';
+import './PlaceForm.css';
 const DUMMY_PLACES = require('../../shared/data/places.json');
-const UpdatePlace = props => {
+const UpdatePlace = () => {
     const placeId = useParams().placeId;
-
-    const identifiedPlace = DUMMY_PLACES.find(place => place.id === placeId);
-
-    if (!identifiedPlace) {
-        return (
-            <div className="center">
-                <h2>Could not find place!</h2>
-            </div>
-        );
-    }
-    
-    return (
-        <form>
-            <Input
-                id="title"
-                element="Input"
-                type="text"
-                label="Title"
-                validators={[VALIDATOR_REQUIRE()]}
-                errorText="Please enter a valid title."
-                onInput={() => { }}
-                value={identifiedPlace.title}
-                valid={true}
-            />
-            <Input
-                id="description"
-                element="textarea"
-                label="Description"
-                validators={[VALIDATOR_MINLENGTH(5)]}
-                errorText="Please enter a valid description (at least 5 characters)."
-                onInput={() => {}}
-                value={identifiedPlace.description}
-                valid={true}
-            />
-            <Button type="submit" disabled={true}>UPDATE PLACE</Button>
-        </form>
+  
+    const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+  
+    const [formState, inputHandler] = useForm(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true
+        },
+        description: {
+          value: identifiedPlace.description,
+          isValid: true
+        }
+      },
+      true
     );
-}
+  
+    const placeUpdateSubmitHandler = event => {
+      event.preventDefault();
+      console.log(formState.inputs);
+    };
+  
+    if (!identifiedPlace) {
+      return (
+        <div className="center">
+          <h2>Could not find place!</h2>
+        </div>
+      );
+    }
+  
+    return (
+      <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title."
+          onInput={inputHandler}
+          initialValue={formState.inputs.title.value}
+          initialValid={formState.inputs.title.isValid}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (min. 5 characters)."
+          onInput={inputHandler}
+          initialValue={formState.inputs.description.value}
+          initialValid={formState.inputs.description.isValid}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          UPDATE PLACE
+        </Button>
+      </form>
+    );
+  };
 
 export default UpdatePlace;
