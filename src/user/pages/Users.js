90c6680cdf91f2d83dd85c493b4adfaaf2833a69
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-13 12:32:57 -0700

    Getting Users with GET Request

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UsersList from '../components/UsersList';
+import config from "../../shared/config/config.json";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
+const backend = config.backend;
+const backendServerUrl = backend.baseServerUrl + ":" + backend.port;
+const userRoute = backend.userRoute;
+
 const Users = () => {
+
+  const getUserUrl = backendServerUrl + userRoute.baseUrl + userRoute.getUser.path;
+  console.log(getUserUrl);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(getUserUrl);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        console.log(responseData.users);
+        setLoadedUsers(responseData.users);
+
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
 
   const USERS = [
     {
@@ -12,7 +56,17 @@ const Users = () => {
     }
   ];
 
-  return <UsersList items={USERS}/>;
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && 
+      <div className="center"> 
+        <LoadingSpinner asOverlay />
+      </div>}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+
+  );
 };
 
 export default Users;
