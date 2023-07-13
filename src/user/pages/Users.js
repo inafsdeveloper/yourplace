7bf819f7cfbde2214df79dba0c9baebcb28ff73c
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-13 13:38:14 -0700

    Added custom-hook to GET users

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,6 +4,7 @@ import UsersList from '../components/UsersList';
 import config from "../../shared/config/config.json";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const backend = config.backend;
 const backendServerUrl = backend.baseServerUrl + ":" + backend.port;
@@ -12,52 +13,30 @@ const userRoute = backend.userRoute;
 const Users = () => {
 
   const getUserUrl = backendServerUrl + userRoute.baseUrl + userRoute.getUser.path;
-  
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState();
 
 
 
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const fetchUsers = async () => {
       try {
-        const response = await fetch(getUserUrl);
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
-        
+        const responseData = await sendRequest(getUserUrl);
         setLoadedUsers(responseData.users);
-
       } catch (err) {
-        setError(err.message);
+        console.log("error");
       }
-      setIsLoading(false);
     };
-    sendRequest();
+    
+    fetchUsers();
 
-  }, []);
+  }, [sendRequest]);
 
-  const errorHandler = () => {
-    setError(null);
-  };
-
-  const USERS = [
-    {
-      id: 'u1',
-      name: 'Inafs Developer',
-      image: 'https://clipart-library.com/newhp/kissclipart-computer-geek-cartoon-clipart-geek-nerd-clip-art-d978f3a27174b0f9.png',
-      places: 3
-    }
-  ];
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && 
       <div className="center"> 
         <LoadingSpinner asOverlay />
